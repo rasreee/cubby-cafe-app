681ea9d9337337112aca8d61f4f2a3a320d745bd
rasreee <lesleychang@berkeley.edu>
2020-11-28 01:42:22 -0800

    lint / format config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { getLinkPreview } from 'link-preview-js';
+
+async function getJSON(url: string) {
+    const json = await getLinkPreview(url, {
+        imagesPropertyType: 'og', // fetches only open-graph images
+        headers: {
+            'user-agent': 'googlebot', // fetches with googlebot crawler user agent
+            'Accept-Language': 'fr-CA', // fetches site for French language
+            // ...other optional HTTP request headers
+        },
+    }).then((data: any) => {
+        console.log('ASDFIJAS;DLFIJK');
+        console.log(data);
+        return data;
+    });
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App"></div>;
 }
 
 export default App;
